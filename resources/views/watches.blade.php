7ef0af5c96103f3349eccad20f2d4279daa09119
krimmelz <krimmelzwaan@st.noorderpoort.nl>
2023-11-14 00:39:25 +0100

    fix filter watches page colliding with eachother when small enough

--- resources/views/watches.blade.php
+++ resources/views/watches.blade.php
@@ -5,7 +5,12 @@
                     <h1 class="font-extrabold text-5xl">Watches.</h1>
                     <a href="/" class="text-[#BABABA]">Read about the materials</a>
                 </div>
-            <div class="flex justify-between w-[30%]">
+            <div class="inline-block md:hidden hover:cursor-pointer">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z" />
+                </svg>
+            </div>
+            <div class="hidden justify-between w-[30%] md:flex">
                 <h1>Filter:</h1>
                 <div class="flex flex-col">
                     <h1>Strap colours</h1>
